9d150d1081225696e7cd0c4b6b630776254e0b2c
Rupnil Codes <rupnil.mondal2@gmail.com>
2026-03-12 16:18:26 +0530

    Initial commit

---
+++ src/main/java/com/rupnilcodes/magicalTools/MagicalTools.java
@@ -0,0 +1,10 @@
+package com.rupnilcodes.magicalTools;
+
+import net.fabricmc.api.ModInitializer;
+
+public class MagicalTools implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
